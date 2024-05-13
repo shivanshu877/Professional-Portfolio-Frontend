fa8912cbcfea82304f79fea38b95769dcc174f0b
shivanshu877 <vashudev8777@gmail.com>
2024-05-14 00:52:26 +0530

    some ui changes

--- src/client.js
+++ src/client.js
@@ -2,12 +2,13 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
-  dataset: process.env.REACT_APP_SANITY_DATASET,
+  projectId: "q0wgfrpj",
+  dataset: "production",
   useCdn: true,
   apiVersion: "2023-05-03",
 
-  token: process.env.REACT_APP_SANITY_TOKEN,
+  token:
+    "skrm9xHBWBD7Hfm4mLTkJfUlEnKEgkSj5cM2wclXXnj8ADrAdAD2pB9PUSCEWvvlkRDEaLUM2ql3jyr4RnZg9ggCgppv8zdaLcAVwPTG8dAFKbmo0qdaE0o3qfJoUNA8rp0g179E3bLUfQo5GYujwj1IaOidwmKvteyug3OYOoh6W3YA7fmR",
 });
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
