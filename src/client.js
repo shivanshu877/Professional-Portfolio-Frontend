a10da3a5a39ae37939f75e391e5d3585736d6ef6
shivanshu877 <vashudev8777@gmail.com>
2024-05-14 00:54:06 +0530

    security added

--- src/client.js
+++ src/client.js
@@ -2,13 +2,12 @@ import { createClient } from "@sanity/client";
 import imageUrlBuilder from "@sanity/image-url";
 
 export const client = createClient({
-  projectId: "q0wgfrpj",
-  dataset: "production",
+  projectId: process.env.REACT_APP_SANITY_PROJECT_ID,
+  dataset: process.env.REACT_APP_SANITY_DATASET,
   useCdn: true,
   apiVersion: "2023-05-03",
 
-  token:
-    "skrm9xHBWBD7Hfm4mLTkJfUlEnKEgkSj5cM2wclXXnj8ADrAdAD2pB9PUSCEWvvlkRDEaLUM2ql3jyr4RnZg9ggCgppv8zdaLcAVwPTG8dAFKbmo0qdaE0o3qfJoUNA8rp0g179E3bLUfQo5GYujwj1IaOidwmKvteyug3OYOoh6W3YA7fmR",
+  token: process.env.REACT_APP_SANITY_TOKEN,
 });
 const builder = imageUrlBuilder(client);
 export const urlFor = (source) => builder.image(source);
